fd410bb71b68cbc2e496c2b0a2c657833220e577
Miron Jakubowski <miron.jakubowski@uniquesoft.eu>
2015-12-02 11:25:58 +0100

    day 1

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day1 = new Day1();
+            Console.WriteLine("----------------Advent of Code------------");
+            Console.WriteLine("---------------------Day 1----------------");
+            Console.WriteLine("Puzzle 1 output: {0}", day1.SovlePuzzle1());
+            Console.WriteLine("Puzzle 2 output: {0}", day1.SolvePuzzle2());
+
+            Console.ReadLine();
+        }
+    }
+}

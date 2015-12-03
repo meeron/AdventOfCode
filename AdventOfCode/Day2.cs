011d233efda10e39b7872706153b430ffa46eb7c
Miron Jakubowski <miron.jakubowski@uniquesoft.eu>
2015-12-03 17:12:04 +0100

    Day2 - Puzzle 2: in progress

---
+++ AdventOfCode/Day2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Day2
+    {
+        private readonly string[] _input;
+
+        public Day2()
+        {
+            _input = File.ReadAllLines(@"c:\Users\miron\Documents\Visual Studio 2013\Projects\AdventOfCode\inputs\Day2-input.txt");
+        }
+
+        public int SolvePuzzle1()
+        {
+            return this._input.Select(x => Dimension.Parse(x)).Sum(x => x.Area);
+        }
+
+        public int SolvePuzzle2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    struct Dimension
+    {
+        public int L;
+        public int W;
+        public int H;
+
+        //in format: LxWxH 
+        public static Dimension Parse(string input)
+        {
+            string[] arr = input.Split('x');
+            if (arr.Length != 3)
+                throw new ArgumentException("Invalid input for dimension.");
+
+            var result = new Dimension();
+            int.TryParse(arr[0], out result.L);
+            int.TryParse(arr[1], out result.W);
+            int.TryParse(arr[2], out result.H);         
+
+            return result;
+        }
+
+        private int[] Sides { get { return new int[] { L * W, W * H, H * L }; } }
+
+        public int Area
+        {
+            get
+            {
+                return this.Sides.Select(x => x * 2).Sum() + this.Sides.Min();
+            }
+        }
+
+        public int Volume { get { return L * W * H; } }
+
+        //public int Ribbon
+        //{
+        //    get
+        //    {
+
+        //    }
+        //}
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,10 +11,15 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             var day1 = new Day1();
+            var day2 = new Day2();
+
             Console.WriteLine("----------------Advent of Code------------");
             Console.WriteLine("---------------------Day 1----------------");
-            Console.WriteLine("Puzzle 1 output: {0}", day1.SovlePuzzle1());
+            Console.WriteLine("Puzzle 1 output: {0}", day1.SolvePuzzle1());
             Console.WriteLine("Puzzle 2 output: {0}", day1.SolvePuzzle2());
+            Console.WriteLine("---------------------Day 2----------------");
+            Console.WriteLine("Puzzle 1 output: {0}", day2.SolvePuzzle1());
+            //Console.WriteLine("Puzzle 2 output: {0}", day1.SolvePuzzle2());
 
             Console.ReadLine();
         }
